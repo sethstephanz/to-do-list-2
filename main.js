de5e922c8066f1b848949195ed02693cda72b6f4
tithecellar <sethstephanz@gmail.com>
2020-12-17 17:02:23 -0500

    Thursday update

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
-let addToDoButton = document.getElementById('addToDo');
-let addByWhenButton = document.getElementById('addByWhen');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
+let addToDoButton = document.getElementById("addToDo");
+let addByWhenButton = document.getElementById("deadline");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
 
-addToDoButton.addEventListener('click', function(){
-    let paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = '';
-    paragraph.addEventListener('click', function() {
-        paragraph.style.textDecoration = 'line-through';
-    });
-    paragraph.addEventListener('dblclick', function() {
-        toDoContainer.removeChild(paragraph);
-    })
+addToDoButton.addEventListener("click", function () {
+  let paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
 });
 
-addByWhenButton.addEventListener('click', function() {
-    let paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    byWhenContainer.appendChild(paragraph);
-    inputField.value = '';
-    paragraph.addEventListener('click', function() {
-        paragraph.style.textDecoration = 'line-through';
-    });
-    paragraph.addEventListener('dblclick', function() {
-        toDoContainer.removeChild(paragraph);
-    })
-});
+addByWhenButton.addEventListener("click", function () {});
